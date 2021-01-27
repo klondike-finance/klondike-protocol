69646dc3279489a0fe45972d417e2f328f1d5c28
0x112358 <0x112358@protonmail.com>
2021-01-27 14:25:42 +0300

    added utils

---
+++ scripts/utils.ts
@@ -0,0 +1,9 @@
+import { ethers } from "ethers";
+
+function encodeParameters(
+  types: Array<string | ParamType>,
+  values: Array<any>
+) {
+  const abi = new ethers.utils.AbiCoder();
+  return abi.encode(types, values);
+}
